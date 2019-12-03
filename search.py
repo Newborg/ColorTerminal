739a38d0510ddfee0980df2f95d14079dc94b94e
Kasper Nyborg Nielsen <knn@foss.dk>
2019-12-03 19:02:08 +0100

    Added selected color to search tags

--- search.py
+++ search.py
@@ -80,10 +80,17 @@ class Search:
             self._view = tk.Frame(self._textField,highlightthickness=2,highlightcolor=self._settings.get(Sets.THEME_COLOR))
             self._view.place(relx=1,x=-5,y=5,anchor=tk.NE)
 
-            self._textField.tag_configure(self.TAG_SEARCH_SELECT_BG, background=self._settings.get(Sets.SEARCH_SELECTED_LINE_COLOR))
-            self._textField.tag_configure(self.TAG_SEARCH, background=self._settings.get(Sets.SEARCH_MATCH_COLOR))
-            self._textField.tag_configure(self.TAG_SEARCH_SELECT, background=self._settings.get(Sets.SEARCH_SELECTED_COLOR))
-
+            darkenColorBy = -30
+            self._textField.tag_configure(self.TAG_SEARCH_SELECT_BG, \
+                                            background=self._settings.get(Sets.SEARCH_SELECTED_LINE_COLOR), \
+                                            selectbackground=self._lightOrDarkenColor(self._settings.get(Sets.SEARCH_SELECTED_LINE_COLOR),darkenColorBy))            
+            self._textField.tag_configure(self.TAG_SEARCH, \
+                                            background=self._settings.get(Sets.SEARCH_MATCH_COLOR), \
+                                            selectbackground=self._lightOrDarkenColor(self._settings.get(Sets.SEARCH_MATCH_COLOR),darkenColorBy))
+            self._textField.tag_configure(self.TAG_SEARCH_SELECT, \
+                                            background=self._settings.get(Sets.SEARCH_SELECTED_COLOR),\
+                                            selectbackground=self._lightOrDarkenColor(self._settings.get(Sets.SEARCH_SELECTED_COLOR),darkenColorBy))
+            
             self._var = tk.StringVar(self._view)
             self._var.set("")
             self._var.trace("w",self._searchStringUpdated)
@@ -373,3 +380,13 @@ class Search:
     def _disableGuiScrolling(self):
         if self._guiWorker:
             self._guiWorker.disableScrolling()
+    
+    def _lightOrDarkenColor(self,hex_color, brightness_offset=1):
+        """ takes a color like #87c95f and produces a lighter or darker variant """
+        if len(hex_color) != 7:
+            raise Exception("Passed %s into _lightOrDarkenColor(), needs to be in #87c95f format." % hex_color)
+        rgb_hex = [hex_color[x:x+2] for x in [1, 3, 5]]
+        new_rgb_int = [int(hex_value, 16) + brightness_offset for hex_value in rgb_hex]
+        new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int] # make sure new values are between 0 and 255        
+        return "#" + "".join('%02x'%i for i in new_rgb_int)
+        
