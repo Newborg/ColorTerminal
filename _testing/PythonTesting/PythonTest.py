947acda4b06618482dc8bee0c7b78b07d63f5d63
Kasper Nielsen <kaspernyborg@gmail.com>
2019-12-08 16:46:54 +0100

    Intermediate commit

--- _testing/PythonTesting/PythonTest.py
+++ _testing/PythonTesting/PythonTest.py
@@ -9,8 +9,10 @@ parser = argparse.ArgumentParser()
 
 
 parser.add_argument("-c","--enableConsole",help="send stdout and stderr to console, otherwise this is written to " + stdoutFile,action="store_true")
-parser.add_argument("-f","--logFilePath",metavar="",help="Path of logfile to open")
+parser.add_argument("logFilePath",metavar="Path",help="Path of logfile to open")
 args = parser.parse_args()
 
 if (args.logFilePath):
     print("File: " + args.logFilePath)
+
+input("Press Enter to continue...")
